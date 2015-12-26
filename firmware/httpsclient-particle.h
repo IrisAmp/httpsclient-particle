ebd1a8de584ea0f1f7321a3530da98c74d0206f7
IrisAmp <brandon.d.yue@gmail.com>
2015-12-26 12:52:27 -0700

    Started work on updating this submodule

--- firmware/httpsclient-particle.h
+++ firmware/httpsclient-particle.h
@@ -8,22 +8,21 @@ extern "C" {
 
 #include "matrixsslApi.h"
 
-#define HTTPS_COMPLETE 1
-#define HTTPS_ERROR -1
-#define ALLOW_ANON_CONNECTIONS 1
+#define HTTPS_COMPLETE          1
+#define HTTPS_ERROR            -1
+#define ALLOW_ANON_CONNECTIONS  1
 #define LOGGING_DEBUG
 
 #define USE_RSA_CIPHER_SUITE
 #define ID_RSA
 
-extern TCPClient client;
-extern bool g_https_complete;
-extern uint32 g_bytes_received;
+extern TCPClient  client;
+extern bool       g_https_complete;
+extern uint32_t   g_bytes_received;
 extern const bool g_https_trace;
 
-int httpsclientSetup(const char * host, const char * path);
-int httpsClientConnection(unsigned char * requestContent, uint32 msg_len,
-			  const char * msg);
+int  httpsclientSetup(const char * host, const char * path);
+int  httpsClientConnection(unsigned char * requestContent, uint32 msg_len, const char * msg);
 void httpsclientCleanUp();
 void httpsclientSetPath(const char * path);
 
